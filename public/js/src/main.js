130bfb418eda9b54b85f74a4d9f36d4197ba5559
dastangoo <hamed.dastangoo@gmail.com>
2017-06-09 17:22:09 +0430

    Just a commit

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,6 +1,7 @@
 import searchComponent from './components/search.js';
 
 function main(){
+    alert('foot');
     searchComponent()
         .init()
         .render(document.querySelector('.search'));
